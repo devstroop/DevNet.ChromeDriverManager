3057a78e47463f12374996fc209a050916200fa6
Akash Shaw <aksbju@gmail.com>
2022-06-12 00:50:25 +0530

    Add project files.

---
+++ DevNet.ChromeDriverManager/ChromeDriverManager.cs
@@ -0,0 +1,118 @@
+﻿using System.IO.Compression;
+
+namespace DevNet
+{
+    public class ChromeDriverManager
+    {
+        private static string _LOCATION = System.IO.Directory.GetCurrentDirectory();
+        private static string ARCHIVE_PATH
+        {
+            get
+            {
+                return System.IO.Path.Combine(_LOCATION ?? System.IO.Directory.GetCurrentDirectory(), "chromedriver.zip").Replace("\\", "/");
+            }
+        }
+        private static string ARCHIVE_DIRECTORY
+        {
+            get
+            {
+                return _LOCATION ?? System.IO.Directory.GetCurrentDirectory();
+            }
+        }
+        private static string CHROMEDRIVER_PATH
+        {
+            get
+            {
+                return System.IO.Path.Combine(_LOCATION ?? System.IO.Directory.GetCurrentDirectory(), "chromedriver.exe").Replace("\\", "/");
+            }
+        }
+        public static string LOCATION
+        {
+            set
+            {
+                _LOCATION = value;
+            }
+        }
+
+
+        private static int? _DOWNLOAD_PROGRESS_PERCENTAGE = null;
+        public static int? DOWNLOAD_PROGRESS_PERCENTAGE
+        {
+            get { return _DOWNLOAD_PROGRESS_PERCENTAGE; }
+        }
+        public static string LATEST_VERSION
+        {
+            get
+            {
+                try
+                {
+                    using (System.Net.WebClient client = new System.Net.WebClient())
+                    {
+                        return client.DownloadString("https://chromedriver.storage.googleapis.com/LATEST_RELEASE");
+                    }
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+        }
+        public static string INSTALLED_VERSION
+        {
+            get
+            {
+                try
+                {
+                    if (System.IO.File.Exists(CHROMEDRIVER_PATH))
+                        return Utility.ExecuteCommandSync($"{CHROMEDRIVER_PATH} -v").Split(' ')[1];
+                }
+                catch { }
+                return null;
+            }
+        }
+
+        private static System.Net.WebClient client;
+
+        public static void InstallOrUpdate()
+        {
+            Uninstall();
+            using (client = new System.Net.WebClient())
+            {
+                System.Uri uri = new System.Uri("https://chromedriver.storage.googleapis.com/" + LATEST_VERSION + "/chromedriver_win32.zip");
+                client.DownloadProgressChanged += DownloadProgressChanged;
+                client.DownloadFileAsync(uri, ARCHIVE_PATH);
+            }
+        }
+
+        public static void Uninstall()
+        {
+            Utility.DestroyAllChromeDrivers();
+            try { System.IO.File.Delete(ARCHIVE_PATH); } catch { }
+            try { System.IO.File.Delete(CHROMEDRIVER_PATH); } catch { }
+        }
+
+        private static void DownloadProgressChanged(object sender, System.Net.DownloadProgressChangedEventArgs e)
+        {
+            if (e.ProgressPercentage < 100)
+            {
+                _DOWNLOAD_PROGRESS_PERCENTAGE = e.ProgressPercentage;
+            }
+            else
+            {
+                Utility.WaitForFileToBeReady(ARCHIVE_PATH, System.TimeSpan.FromSeconds(30));
+                using (System.IO.Compression.ZipArchive archive = System.IO.Compression.ZipFile.OpenRead(ARCHIVE_PATH))
+                {
+                    foreach (System.IO.Compression.ZipArchiveEntry entry in archive.Entries)
+                    {
+                        try { System.IO.File.Delete(System.IO.Path.Combine(ARCHIVE_DIRECTORY, entry.FullName)); } catch { }
+                        entry.ExtractToFile(System.IO.Path.Combine(ARCHIVE_DIRECTORY, entry.FullName));
+                    }
+                }
+                System.IO.File.Delete(ARCHIVE_PATH);
+                _DOWNLOAD_PROGRESS_PERCENTAGE = null;
+                client.DownloadProgressChanged -= DownloadProgressChanged;
+            }
+
+        }
+    }
+}
---
+++ DevNet.ChromeDriverManager/Utility.cs
@@ -0,0 +1,65 @@
+﻿namespace DevNet
+{
+    public class Utility
+    {
+        public async static void DestroyAllChromeDrivers()
+        {
+            try
+            {
+                System.Diagnostics.ProcessStartInfo cmdsi = new System.Diagnostics.ProcessStartInfo("taskkill.exe");
+                cmdsi.Arguments = "/F /IM chromedriver.exe /T";
+                System.Diagnostics.Process cmd = System.Diagnostics.Process.Start(cmdsi);
+                cmd.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                cmd.StartInfo.CreateNoWindow = true;
+                cmd.WaitForExit();
+            }
+            catch { }
+            await System.Threading.Tasks.Task.Run(async delegate
+            {
+                await System.Threading.Tasks.Task.Delay(500);
+            });
+        }
+        public static string ExecuteCommandSync(object command)
+        {
+            try
+            {
+                System.Diagnostics.ProcessStartInfo procStartInfo = new System.Diagnostics.ProcessStartInfo("cmd", "/c " + command);
+                procStartInfo.RedirectStandardOutput = true;
+                procStartInfo.UseShellExecute = false;
+                procStartInfo.CreateNoWindow = true;
+                System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                proc.StartInfo = procStartInfo;
+                proc.Start();
+                return proc.StandardOutput.ReadToEnd();
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        public static bool IsFileReady(string filename)
+        {
+            // If the file can be opened for exclusive access it means that the file
+            // is no longer locked by another process.
+            try
+            {
+                using (System.IO.FileStream inputStream = System.IO.File.Open(filename, System.IO.FileMode.Open, System.IO.FileAccess.Read, System.IO.FileShare.None))
+                    return inputStream.Length > 0;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public static void WaitForFileToBeReady(string filename, System.TimeSpan timeout)
+        {
+            System.DateTime startedOn = System.DateTime.Now;
+            while (!IsFileReady(filename))
+            {
+                if (startedOn.AddMilliseconds(timeout.Milliseconds) >= System.DateTime.Now)
+                    break;
+            }
+        }
+
+    }
+}
